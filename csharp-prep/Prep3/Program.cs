8d1e8440046e11573f659e5df59cc5b2dce6fc53
Akinwumidi <israelakin14@gmail.com>
2024-01-14 10:34:49 +0100

    Complete: Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("NUMBER GUESSING GAME");
+
+
+        Random randomNum = new Random();
+        int magicNumber = randomNum.Next(1, 20);
+
+        int answer = 0;
+        int guessTimes = 1;
+
+        while(answer != 5 && guessTimes <4) {
+            Console.Write("What is youŕ guess?");
+            string guess = Console.ReadLine();
+
+            if( int.Parse(guess)== answer ){
+                Console.WriteLine("You guessed it");
+            }
+            if(int.Parse(guess)>answer){
+                Console.WriteLine("Higher", "you have "+ (guessTimes+1) + " guesses left");
+            }
+            if(int.Parse(guess)<answer){
+                Console.Write("Lower");
+                Console.WriteLine("Higher", "you have "+ (guessTimes+1) + " guesses left");
+            }
+            guessTimes+=1;
+        }
     }
 }
