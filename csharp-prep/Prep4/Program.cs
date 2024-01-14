a3960da2501f122f3a065cd423ce8d3199bb1ed6
Akinwumidi <israelakin14@gmail.com>
2024-01-14 18:08:33 +0100

    Complete: Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int userInput = -1;
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userInput = int.Parse(userResponse);
+            
+            // Only add the number to the list if it is not 0
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        } while (userInput != 0);
+
+        // Part 1: Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Part 2: Compute the average 
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        int maxNumber = numbers.Count > 0 ? numbers.Max() : 0; 
+
+        // Console.WriteLine($"The max is: {maxNumber}");
+        Console.WriteLine($"The max is: {maxNumber}");
     }
 }
