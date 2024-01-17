cfa4672049617db8aa423e1102656412ee5bac76
Akinwumidi <israelakin14@gmail.com>
2024-01-17 17:06:24 +0100

    Complete: W02 Resume Program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+ 
+    class Program
     {
-        Console.WriteLine("Hello Learning02 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("############ Learning Activity W02 #############");
+            Console.WriteLine("------------------------------------------------");
+
+            Job job1 = new Job();
+            job1._jobTitle = "Frontend Developer";
+            job1._companyName = "Techclub NG";
+            job1._startYear= 2020;
+            job1._endYear= 2022;
+
+            Job job2 = new Job();
+            job2._jobTitle = "Development Team Lead";
+            job2._companyName ="XeraxLabs";
+            job2._startYear = 2020;
+            job2._endYear = 2024;
+
+
+            Resume myResume = new Resume();
+            myResume._personName = "Israel Akinwumi";
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
+
+            myResume.Display();
+
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("############ Learning Activity W02 #############");
+        }
     }
-}
+ 
+
+
+
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,13 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
+    static void Main(string[] args){
+    
+    string myName ="Israel Akinwumi";
+    const int age = 26;
+
+    Console.WriteLine("##################################");
+    Console.WriteLine("  PLAYING AROUND IN THE SANDBOX");
+    Console.WriteLine("##################################"); 
+    Console.WriteLine($"My name is {myName} and i am {age} years old");
     }
 }
